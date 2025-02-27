f1b38c24670afdf1b2bc95aaa1b1f753f72c92d7
Oliver-Timmerlid <113107596+Oliver-Timmerlid@users.noreply.github.com>
2025-02-27 15:15:42 +0100

    Added update user to settingspage

--- ViewModel/SettingPageViewModel.cs
+++ ViewModel/SettingPageViewModel.cs
@@ -1,12 +1,58 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Java.Util;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Android.Bluetooth;
+using Android.Content;
+using Android.DeviceLock;
+using Android.Telephony;
+using Android.Provider;
+using CommunityToolkit.Mvvm.Input;
 
 namespace MauiApp1.ViewModel
 {
-    public class SettingPageViewModel
+    public partial class SettingPageViewModel : ObservableObject
     {
+        private readonly FirestoreService _firestoreService;
+        private readonly string serviceUuid;
+        private User user;
+
+        [ObservableProperty]
+        private string currentName;
+
+        [ObservableProperty]
+        private string changedName;
+
+        public SettingPageViewModel(FirestoreService firestoreService)
+        {
+            _firestoreService = firestoreService;
+            serviceUuid = GetAndroidIdToUuid();
+            GetCurrentUser();
+        }
+        private string GetAndroidIdToUuid()
+        {
+            var context = Android.App.Application.Context;
+            var androidId = Settings.Secure.GetString(context.ContentResolver, Settings.Secure.AndroidId);
+            return UUID.NameUUIDFromBytes(Encoding.UTF8.GetBytes(androidId)).ToString();
+        }
+        private async void GetCurrentUser()
+        {
+            User localUser = await _firestoreService.GetUser(serviceUuid);
+            CurrentName = localUser.Name;
+            user = localUser;
+        }
+
+        [RelayCommand]
+        private async Task SaveButton()
+        {
+            user.Name = ChangedName;
+            await _firestoreService.UpdateUser(user);
+            //user = await _firestoreService.GetUser(serviceUuid);
+            //CurrentName = user.Name;
+            GetCurrentUser();
+        }
     }
 }
